5c7bf4b25c1e8a5b5d8d7bab2da0bd9db51e0d50
Vladimir Zhuravlev <izhur27@gmail.com>
2019-12-25 18:53:50 +0300

    webpack config actualised

--- webpack.config.js
+++ webpack.config.js
@@ -3,25 +3,14 @@ const path = require('path');
 const main = (name, minimize) => ({
     entry: './src/index.js',
     mode: "production",
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-        ],
-    },
     optimization: {
         minimize,
         usedExports: true
     },
     resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
+        extensions: ['.js'],
     },
     output: {
-        library: 'dApp',
-        libraryTarget: "umd",
         globalObject: "this",
         filename: name,
         path: path.resolve(__dirname, 'dist'),
