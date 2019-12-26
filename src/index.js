4c2723f0f98f5d9bad9b313d5f70a922a0364a2a
Vladimir Zhuravlev <izhur27@gmail.com>
2019-12-26 17:23:49 +0300

    txs fixes & testnet provider

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import Waves from "@waves/signer";
 import Provider from "@waves.exchange/provider-web";
 
 const waves = new Waves({NODE_URL: 'https://pool.testnet.wavesnodes.com'});
-const provider = new Provider();
+const provider = new Provider('https://testnet.waves.exchange/signer/');
 
 waves.setProvider(provider);
 
 
-document.querySelector(".js-login").addEventListener("click", async function(event) {
+document.querySelector(".js-login").addEventListener("click", async function (event) {
     try {
         const userData = await waves.login();
         event.target.classList.add("clicked");
@@ -22,7 +22,7 @@ document.querySelector(".js-login").addEventListener("click", async function(eve
 
 // calling a "faucet" script wavesexplorer.com/tesnet/address/3MuN7D8r19zdvSpAd1L91Gs88bcgwUFy2mn/script
 // this will top up the account balance, but only once
-document.querySelector(".js-invoke").addEventListener("click", function() {
+document.querySelector(".js-invoke").addEventListener("click", function () {
     waves.invoke({
         dApp: "3MuN7D8r19zdvSpAd1L91Gs88bcgwUFy2mn",
         call: {
@@ -33,21 +33,25 @@ document.querySelector(".js-invoke").addEventListener("click", function() {
 
 
 // just putting some data into account storage
-document.querySelector(".js-data").addEventListener("click", function() {
+document.querySelector(".js-data").addEventListener("click", function () {
+    const date = new Date();
     waves.data({
         data: [
-            {key: "lastCall", value: new Date(), type: 'string'}
+            {
+                key: "lastCall",
+                value: `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`,
+                type: 'string'
+            }
         ]
     }).broadcast().then(console.log)
 });
 
 
 // just transferring some WAVES token to Alice
-document.querySelector(".js-transfer").addEventListener("click", function() {
+document.querySelector(".js-transfer").addEventListener("click", function () {
     waves.transfer({
         recipient: "3MuN7D8r19zdvSpAd1L91Gs88bcgwUFy2mn",
-        amount: 1,
-        attachment: "Happy New Year!"
+        amount: 1
     }).broadcast().then(console.log)
 });
 
